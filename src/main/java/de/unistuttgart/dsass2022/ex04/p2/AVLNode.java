4c7b8870056b288faac744048d00ebfd31068e50
marvin <marvinmai@posteo.de>
2022-05-11 18:43:30 +0200

    Implement AVLNode

---
+++ src/main/java/de/unistuttgart/dsass2022/ex04/p2/AVLNode.java
@@ -0,0 +1,57 @@
+package de.unistuttgart.dsass2022.ex04.p2;
+
+public class AVLNode<T extends Comparable<T>> implements IAVLNode<T> {
+
+	private IAVLNode<T> left;
+	private IAVLNode<T> right;
+
+	private T value;
+	private int balance;
+
+	public AVLNode() {
+		this.left = null;
+		this.right = null;
+		this.value = null;
+	}
+
+	@Override
+	public void setValue(T val) {
+		this.value = val;
+	}
+
+	@Override
+	public T getValue() {
+		return value;
+	}
+
+	@Override
+	public void setLeftChild(IAVLNode<T> left) {
+		this.left = left;
+	}
+
+	@Override
+	public IAVLNode<T> getLeftChild() {
+		return left;
+	}
+	
+	@Override
+	public void setRightChild(IAVLNode<T> right) {
+		this.right = right;
+	}
+
+	@Override
+	public IAVLNode<T> getRightChild() {
+		return right;
+	}
+
+	@Override
+	public void setBalance(int bal) {
+		this.balance = bal;
+	}
+
+	@Override
+	public int getBalance() {
+		return balance;
+	}
+
+}
